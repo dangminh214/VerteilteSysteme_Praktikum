1efc3c45703d97904a5187fc157196aa4007258f
dangminh214 <dangminh214@gmail.com>
2023-06-12 13:45:05 +0200

    HTTP, TCP and Client

--- src/main/java/org/borse/RandomValueForWertpapier.java
+++ src/main/java/org/borse/RandomValueForWertpapier.java
@@ -5,10 +5,8 @@ public class RandomValueForWertpapier {
 
     private static final Random random = new Random();
 
-
     public RandomValueForWertpapier() {
     }
-
 
     public static synchronized int randomizeData(int min, int max) {
         return random.nextInt(max - min + 1) + min;
